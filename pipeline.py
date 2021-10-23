c8d9918c2ec73c14d1037adaedb298b7e577b0f4
CaptRosh <roshan.srivastava01@gmail.com>
2021-10-23 18:57:28 +0530

    newexp included + fixes

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,5 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import bs4
@@ -15,7 +14,7 @@ for folder in ["indianexpress","ndtv","newsexp"]:
 keys = "Lakhimpur"
 
 #NEWSEXP
-global_url = "https://www.newindianexpress.com/topic?term="
+global_url = "https://www.newindianexpress.com/topic?per_page="
 headlines_text=[]
 articles_text = []
 options = Options()
@@ -24,16 +23,17 @@ options.add_argument("--log-level-3")
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
 driver2 = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
 
-for i in range(1,4):
-    driver.get(global_url+ keys +"&request=ALL&search=short")
+for i in range(4):
+    driver.get(f"{global_url}{i}0&term={keys}&request=ALL&search=short")
     page = bs4.BeautifulSoup(driver.page_source, 'html.parser')
     headlines = page.find_all('div', {'class':'search-row_type'})
     for headline in headlines:
-        headlines_text.append(headline.h4.a.text)
-        page= driver2.get(headline.h4.a['href'])
-        page = bs4.BeautifulSoup(driver2.page_source,'lxml')
-        article = page.find('div',{'class':'articlestorycontent'}).find_all('p')
-        articles_text.append(" ".join(i.text for i in article))
+        if headline.h4.text not in headlines_text:
+            headlines_text.append(headline.h4.a.text)
+            page= driver2.get(headline.h4.a['href'])
+            page = bs4.BeautifulSoup(driver2.page_source,'lxml')
+            article = page.find('div',{'class':'articlestorycontent'}).find_all('p')
+            articles_text.append(" ".join(i.text for i in article))
     
 for i in range(min(30,len(headlines_text))):
     with open(f"newsexp/newsexp_{i}.txt",'w') as file:
@@ -54,11 +54,12 @@ for i in range(1,3):
     page = bs4.BeautifulSoup(driver.page_source, 'html.parser')
     headlines = page.find_all('div', {'class':'details'})
     for headline in headlines:
-        headlines_text.append(headline.h3.a.text)
-        page = requests.get(headline.h3.a["href"])
-        page = bs4.BeautifulSoup(page.text,'lxml')
-        article = page.find_all('p')
-        articles_text.append(" ".join(i.text for i in article))
+        if headline.h3.a.text not in headlines_text:
+            headlines_text.append(headline.h3.a.text)
+            page = requests.get(headline.h3.a["href"])
+            page = bs4.BeautifulSoup(page.text,'lxml')
+            article = page.find_all('p')
+            articles_text.append(" ".join(i.text for i in article))
 
 for i in range(len(headlines_text)):
     with open(f"indianexpress/indianexpress_{i}.txt",'w') as file:
@@ -78,11 +79,12 @@ page = bs4.BeautifulSoup(driver.page_source, 'html.parser')
 headlines = page.find_all('div', {'class':'src_itm-ttl'})
 
 for headline in headlines:
-    headlines_text.append(headline.a['title'])
-    page = requests.get(headline.a["href"])
-    page = bs4.BeautifulSoup(page.text,'lxml')
-    article = page.find_all('div','sp-cn ins_storybody')
-    articles_text.append(" ".join(i.text for i in article))
+    if headline.a['title'] not in headlines_text:
+        headlines_text.append(headline.a['title'])
+        page = requests.get(headline.a["href"])
+        page = bs4.BeautifulSoup(page.text,'lxml')
+        article = page.find_all('div','sp-cn ins_storybody')
+        articles_text.append(" ".join(i.text for i in article))
 
 for i in range(len(headlines_text)):
     with open(f"ndtv/ndtv_{i}.txt",'w') as file:
